03d4e4b02cee67a92599e45f70b61a73ea18530e
YolandaThant <yolanda.lee159@gmail.com>
2024-11-30 15:25:16 -0500

    feat added test cases for LoginInteractor
    # Conflicts:
    #	src/test/java/use_case/login/LoginInteractorTest.java

---
+++ src/test/java/use_case/login/LoginInteractorTest.java
@@ -0,0 +1,85 @@
+package use_case.login;
+import entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+
+public class LoginInteractorTest {
+
+    private LoginInteractor loginInteractor;
+    private LoginUserDataAccessInterface mockLoginUserDAI;
+    private LoginOutputBoundary mockLoginOutputBoundary;
+
+
+    @BeforeEach
+    public void mockSetUp() {
+
+        mockLoginUserDAI = Mockito.mock(LoginUserDataAccessInterface.class);
+        mockLoginOutputBoundary = Mockito.mock(LoginOutputBoundary.class);
+        loginInteractor = new LoginInteractor(mockLoginUserDAI, mockLoginOutputBoundary);
+
+    }
+
+    @Test
+    public void successUserLoggedInTest() {
+
+        String username = "userExists";
+        String password = "passwordExists";
+        LoginInputData inputData = new LoginInputData(username, password);
+
+        User mockUser = Mockito.mock(User.class);
+
+        when(mockLoginUserDAI.existsByName(username)).thenReturn(true);
+        when(mockLoginUserDAI.get(username)).thenReturn(mockUser);
+        when(mockUser.getUsername()).thenReturn(username);
+        when(mockUser.getPassword()).thenReturn(password);
+
+        loginInteractor.execute(inputData);
+
+        verify(mockLoginUserDAI).setCurrentUser(username);
+        verify(mockLoginOutputBoundary).prepareSuccessView(any(LoginOutputData.class));
+    }
+
+    @Test
+    public void failureUserDoesNotExistTest() {
+
+        String username = "userDoesNotExist";
+        String password = "passwordDoesNotExist";
+
+        LoginInputData inputData = new LoginInputData(username, password);
+
+        when(mockLoginUserDAI.existsByName(username)).thenReturn(false);
+
+        loginInteractor.execute(inputData);
+
+        verify(mockLoginOutputBoundary).prepareFailView(username + ": Account does not exist.");
+    }
+
+    @Test
+    public void failurePasswordMismatchTest() {
+
+        String username = "currentUser";
+        String password = "passwordMismatch";
+        LoginInputData inputData = new LoginInputData(username, password);
+
+        User mockUser = Mockito.mock(User.class);
+
+        when(mockLoginUserDAI.existsByName(username)).thenReturn(true);
+        when(mockLoginUserDAI.get(username)).thenReturn(mockUser);
+        when(mockUser.getPassword()).thenReturn("correctPassword");
+
+        loginInteractor.execute(inputData);
+
+        verify(mockLoginOutputBoundary).prepareFailView("Incorrect password for \"" + username + "\".");
+    }
+
+    @Test
+    public void switchToSignupViewTest() {
+
+        loginInteractor.switchToSignupView();
+
+        verify(mockLoginOutputBoundary).switchToSignupView();
+    }
+
+}
