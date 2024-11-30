cbba4563606ee7ed8a9010582e0098b2fa23184b
Sataphon Obra <puye53991@gmail.com>
2024-11-30 12:50:38 -0500

    [feature] generate swiping list

--- src/main/java/use_case/chat/ChatInputData.java
+++ src/main/java/use_case/chat/ChatInputData.java
@@ -5,19 +5,19 @@ import entity.ChatMessage;
 public class ChatInputData {
     private final String chatURL;
     private final String currentUser;
-    private final String pfpURL;
+//    private final String pfpURL;
     private final ChatMessage chatMessage;
 
     public ChatInputData(String chatURL, String currentUser, ChatMessage chatMessage) {
         this.chatURL = chatURL;
         this.currentUser = currentUser;
-        this.pfpURL = pfpURL;
+//        this.pfpURL = pfpURL;
         this.chatMessage = chatMessage;
     }
 
-    public String getPfpURL() {
-        return pfpURL;
-    }
+//    public String getPfpURL() {
+//        return pfpURL;
+//    }
 
     public String getCurrentUser() {
         return currentUser;
